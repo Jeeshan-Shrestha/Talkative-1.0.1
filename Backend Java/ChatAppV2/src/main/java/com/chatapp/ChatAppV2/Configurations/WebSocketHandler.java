0da3677aa9a882aacfe894c392ec9c61c39c4aab
Zeeshan Shrestha <shresthajeesan@gmail.com>
2025-06-04 21:17:52 +0545

    added multiple user connection

--- Backend Java/ChatAppV2/src/main/java/com/chatapp/ChatAppV2/Configurations/WebSocketHandler.java
+++ Backend Java/ChatAppV2/src/main/java/com/chatapp/ChatAppV2/Configurations/WebSocketHandler.java
@@ -1,23 +1,28 @@
 package com.chatapp.ChatAppV2.Configurations;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
 
+    private Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<>());
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        // nothing here for now
+        sessions.add(session);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        // nothing here for now
+        sessions.remove(session);
     }
 
     // @Override
@@ -28,7 +33,13 @@ public class WebSocketHandler extends TextWebSocketHandler {
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        session.sendMessage(new TextMessage(message.getPayload()));
+        synchronized (sessions) {
+            for (WebSocketSession wsSession : sessions) {
+                if (wsSession.isOpen()) {
+                    wsSession.sendMessage(message);
+                }
+            }
+        }
     }
 
 }
