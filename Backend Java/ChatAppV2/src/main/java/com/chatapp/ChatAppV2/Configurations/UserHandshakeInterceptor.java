a5f0f399fb33dd9ffc6ac83ff4c22e6b4930ed91
Zeeshan Shrestha <shresthajeesan@gmail.com>
2025-06-05 18:46:42 +0545

    aaa

--- Backend Java/ChatAppV2/src/main/java/com/chatapp/ChatAppV2/Configurations/UserHandshakeInterceptor.java
+++ Backend Java/ChatAppV2/src/main/java/com/chatapp/ChatAppV2/Configurations/UserHandshakeInterceptor.java
@@ -13,10 +13,20 @@ public class UserHandshakeInterceptor implements HandshakeInterceptor {
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
             Map<String, Object> attributes) throws Exception {
         String query = request.getURI().getQuery();
-        if (query != null && query.startsWith("username=")) {
-            String username = query.substring("username=".length());
-            attributes.put("username", username);
+
+        if (query != null) {
+            String[] params = query.split("&");
+            for (String param : params) {
+                String[] pair = param.split("=");
+                if (pair.length == 2 && pair[0].equals("username")) {
+                    String username = java.net.URLDecoder.decode(pair[1], "UTF-8");
+                    System.out.println("Connected username: " + username);
+                    attributes.put("username", username);
+                    break;
+                }
+            }
         }
+
         return true;
     }
 
