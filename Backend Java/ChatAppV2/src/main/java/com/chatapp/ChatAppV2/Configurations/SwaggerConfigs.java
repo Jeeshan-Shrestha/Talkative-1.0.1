0e4263e49f6f0107621e8db01cf737ccc06efeec
Zeeshan Shrestha <shresthajeesan@gmail.com>
2025-07-22 15:54:12 +0545

    fixed minor bug

---
+++ Backend Java/ChatAppV2/src/main/java/com/chatapp/ChatAppV2/Configurations/SwaggerConfigs.java
@@ -0,0 +1,10 @@
+package com.chatapp.ChatAppV2.Configurations;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfigs {
+    
+    
+
+}
---
+++ Backend Java/ChatAppV2/src/main/java/com/chatapp/ChatAppV2/Models/UserDTO.java
@@ -0,0 +1,14 @@
+package com.chatapp.ChatAppV2.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    
+    private String username;
+
+}
