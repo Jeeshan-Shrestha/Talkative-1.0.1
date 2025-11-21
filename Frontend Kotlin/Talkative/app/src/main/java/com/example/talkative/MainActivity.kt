0f222eaa6311b556d24afbdaf9c23477a376c582
Sanskar-Rijal <rijalsanskar@gmail.com>
2025-11-21 19:00:45 +0545

    check

--- Frontend Kotlin/Talkative/app/src/main/java/com/example/talkative/MainActivity.kt
+++ Frontend Kotlin/Talkative/app/src/main/java/com/example/talkative/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : ComponentActivity() {
             TalkativeTheme {
                 myApp {
                     TalkativeNavigation()
+                    //Hello world
                 }
             }
         }
