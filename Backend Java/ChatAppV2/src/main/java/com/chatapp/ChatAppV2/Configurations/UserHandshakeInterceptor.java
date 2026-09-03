9767003e6d18c24b4713a7ef2b84f9b0ca44d9a1
Nara <shresthajeesan@gmail.com>
2026-09-04 00:38:22 +0545

    websocket cookie issue fixed

--- Backend Java/ChatAppV2/src/main/java/com/chatapp/ChatAppV2/Configurations/UserHandshakeInterceptor.java
+++ Backend Java/ChatAppV2/src/main/java/com/chatapp/ChatAppV2/Configurations/UserHandshakeInterceptor.java
@@ -21,35 +21,52 @@ public class UserHandshakeInterceptor implements HandshakeInterceptor {
 
     @Autowired
     private JwtUtils jwt;
-
     @Autowired
     private MyUserDetailsService myUserDetailsService;
 
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
             Map<String, Object> attributes) throws Exception {
-                if (request instanceof ServletServerHttpRequest s){
-                    HttpServletRequest servletRequest = s.getServletRequest();
-                    Cookie[] cookies = servletRequest.getCookies();
-                    if (cookies != null){
-                        for (Cookie cookie : cookies){
-                            if (cookie.getName().equals("token")){
-                                String token = cookie.getValue();
-                                String username = jwt.extractUsername(token);
-                                attributes.put("username", username);
-                            }
-                        }
-                    }
+
+        if (!(request instanceof ServletServerHttpRequest s)) {
+            return false;
+        }
+
+        HttpServletRequest servletRequest = s.getServletRequest();
+        String token = null;
+
+        // 1. Try cookie (web clients)
+        Cookie[] cookies = servletRequest.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    token = cookie.getValue();
+                    break;
                 }
+            }
+        }
+
+        // 2. Fallback: query param ?token=... (Android/OkHttp, Postman)
+        if (token == null) {
+            token = servletRequest.getParameter("token");
+        }
+
+        if (token == null) {
+            return false; // reject handshake, no token found anywhere
+        }
+
+        String username = jwt.extractUsername(token);
+        if (username == null) {
+            return false; // invalid/expired token
+        }
+
+        attributes.put("username", username);
         return true;
     }
 
     @Override
     public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
             Exception exception) {
-
         // to do later
-
     }
-
 }
